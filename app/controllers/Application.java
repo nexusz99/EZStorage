f8eeb79f11047268f7a31968b8e899e8a94c30b0
nexusz99 <nexusz99@gmail.com>
2015-05-08 15:48:15 +0900

    file upload test code

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,28 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
+import java.util.Map;
 
-import views.html.*;
+import play.mvc.Controller;
+import play.mvc.Http.MultipartFormData;
+import play.mvc.Http.MultipartFormData.FilePart;
+import play.mvc.Result;
+import views.html.index;
 
 public class Application extends Controller {
 
     public static Result index() {
         return ok(index.render("Your new application is ready."));
     }
-
+    
+    public static Result file()
+    {
+    	MultipartFormData body = request().body().asMultipartFormData();
+    	FilePart file = body.getFile("uploadFile");
+    	long fileSize = file.getFile().length();
+    	String fileName = file.getFilename();
+    	
+    	String[] tags = body.asFormUrlEncoded().get("tags");
+    	
+    	return ok("filename : " + fileName + " / file size : " + fileSize + " / tags : " + tags);
+    }
 }
