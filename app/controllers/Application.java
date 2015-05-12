438974ccc0ef909be2bde3a1cf91de0a3c9babf1
nexusz99 <nexusz99@gmail.com>
2015-05-12 15:07:39 +0900

    draw file list skeleton code

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -11,4 +11,8 @@ public class Application extends Controller {
         return ok(index.render("Your new application is ready."));
     }
 
+    public static Result files()
+    {
+    	return ok("[{\"filename\": \"test.txt\"}, {\"filename: \"wow.jpg\"}, {\"filename\": \"1234.hwp\"}]");
+    }
 }
