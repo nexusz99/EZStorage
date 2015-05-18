faa3b4226e0179d82f227f4ba00f3d86bcc96c4a
Myoji <myoji1224@gmail.com>
2015-05-18 19:07:40 +0900

    파일 업로드 다이얼로그 구현, 태그 부착, 제거 기능 구현 ( ! 서버와의 데이터 통신 기능은 아직 미구현)

--- public/javascripts/drive.js
+++ public/javascripts/drive.js
@@ -1,3 +1,40 @@
+function resize_fileGrid() // container_fileSystemGrid의 너비를 윈도우 크기에 맞게 리사이징
+{
+    var cont_fileDrive = document.getElementById('container_fileDriveGrid');
+    cont_fileDrive.style.width = window.innerWidth - 300 + 'px';
+    
+    var cont_fileSystem = document.getElementById('container_fileSystemGrid');
+    cont_fileSystem.style.height = window.innerHeight - 175 + 'px';
+}
+    resize_fileGrid();
+    // 브라우저 크기가 변할 시 동적으로 사이즈를 조절
+    window.addEventListener('resize', resize_fileGrid);
+    
+
+// 크롬 브라우저의 fakepath의 출력 해결    
+$(function()
+{
+	$('#btn_fileUpload').change(function()
+	{
+		$('#name_fileUpload').val(this.files && this.files.length ? this.files[0].name : this.value.replace(/^C:\\fakepath\\/i, ''));
+	});
+});
+
+
+// 태그 생성/제거 함수
+$(function()
+{
+	$('#btn_createTag').click(function()
+	{
+		$('#list_fileTag').append('<li><input type="text" class="txt_fileTag"></li>');
+	});
+	
+	$('#btn_deleteTag').click(function()
+	{
+		$('#list_fileTag > li:last').remove();
+	});
+});
+
 function resize_fileGrid() 
 {
     var cont_fileDrive = document.getElementById('container_fileDriveGrid');
