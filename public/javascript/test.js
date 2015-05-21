09e351af09784b9679d295588a2b6854b25cc1d6
Myoji <myoji1224@gmail.com>
2015-05-21 23:15:26 +0900

    no message

--- public/javascript/test.js
+++ public/javascript/test.js
@@ -6,7 +6,6 @@ $(function()
 		dataType:"json",
 		success:function(result)
 		{
-			alert(url);
 			$.each(result.filelist, function(i, d)
 			{
 				$('#testList ol').append('<li> + d["k"] +</li>');
